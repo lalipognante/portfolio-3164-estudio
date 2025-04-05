5d46838e37abd03f6d9d544debc6b13d03abf2b3
Pognante Laura <lalipognante@gmail.com>
2025-04-05 16:56:58 -0300

    Update proyectos.ts

--- data/proyectos.ts
+++ data/proyectos.ts
@@ -1,18 +1,37 @@
-const proyectos = [
+// data/proyectos.ts
+export const proyectos = [
   {
-    titulo: "Casa en Las Sierras",
+    titulo: "Proyecto 1",
+    descripcion: "Casa en las sierras",
     imagenes: [
-      "/proyectos/casa-sierras-1.jpg",
-      "/proyectos/casa-sierras-2.jpg"
+      "/proyectos/image1-project1.jpeg",
+      "/proyectos/image2-project1.jpeg",
+      "/proyectos/image3-project1.jpeg"
     ]
   },
   {
-    titulo: "Centro Cultural, Córdoba",
+    titulo: "Proyecto 2",
+    descripcion: "Casa entre los árboles",
     imagenes: [
-      "/proyectos/ccba-1.jpg"
+      "/proyectos/image1-project2.jpeg",
+      "/proyectos/image2-project2.jpeg"
     ]
   },
-  // Agregá más proyectos acá
+  {
+    titulo: "Proyecto 3",
+    descripcion: "Obra en las sierras",
+    imagenes: [
+      "/proyectos/image1-project3.jpeg",
+      "/proyectos/image2-project3.jpeg"
+    ]
+  },
+  {
+    titulo: "Proyecto 4",
+    descripcion: "Casa suburbana",
+    imagenes: [
+      "/proyectos/image1-project4.jpeg",
+      "/proyectos/image4-project4.jpeg",
+      "/proyectos/image5-project4.jpeg"
+    ]
+  }
 ];
-
-export default proyectos;
