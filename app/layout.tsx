6dcde875b9525b83edd3f9e26694be6666cac29c
Pognante Laura <lalipognante@gmail.com>
2025-04-05 15:49:40 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
+// app/layout.tsx
+import './globals.css';
+
 export const metadata = {
   title: 'Portfolio 31 64 Estudio',
   description: 'Portfolio web para estudio de arquitectura',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="es">
       <body>{children}</body>
