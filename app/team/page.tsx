74dea7f1aaae4cf4b6214c79c2bbfed72b7a6408
Pognante Laura <lalipognante@gmail.com>
2025-04-09 21:45:55 -0300

    Update page.tsx

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -2,7 +2,6 @@
 export default function TeamPage() {
   return (
     <main className="max-w-2xl text-sm">
-      <h2 className="text-lg font-semibold mb-4">Equipo de trabajo</h2>
       <ul className="list-disc list-inside">
         <li>De Biasi Ignacio</li>
         <li>Pognante Mariano</li>
