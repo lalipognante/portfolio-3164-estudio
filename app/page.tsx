246d3c1902fc95e15d43167c087302049702473f
Pognante Laura <lalipognante@gmail.com>
2025-04-10 00:45:27 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -48,15 +48,17 @@ function Proyecto({
         />
       </div>
 
-      <div className="description-container mt-2 text-sm w-full md:w-1/2 flex flex-col items-center justify-center">
-        <p className="text-xs font-mono text-center">{descripcion}</p>
-        {imagenes.length > 1 && (
-          <div className="flex justify-center gap-2 mt-1">
-            <button onClick={prev} className="hover:underline">{"<"}</button>
-            <span>{index + 1} / {imagenes.length}</span>
-            <button onClick={next} className="hover:underline">{">"}</button>
-          </div>
-        )}
+      <div className="description-container mt-2 text-sm w-full md:w-1/2 flex flex-row justify-between items-center">
+        <p className="text-xs font-mono text-left w-3/4">{descripcion}</p>
+        <div className="flex justify-center gap-2 mt-1 w-1/4">
+          {imagenes.length > 1 && (
+            <>
+              <button onClick={prev} className="hover:underline">{"<"}</button>
+              <span>{index + 1} / {imagenes.length}</span>
+              <button onClick={next} className="hover:underline">{">"}</button>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
