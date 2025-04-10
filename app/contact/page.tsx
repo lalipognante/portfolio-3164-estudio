85f77145c95b25b40b3c3c7fc141d95195118d19
Pognante Laura <lalipognante@gmail.com>
2025-04-09 21:45:43 -0300

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,6 @@
 export default function ContactPage() {
   return (
     <main className="max-w-2xl text-sm">
-      <h2 className="text-lg font-semibold mb-4">Contacto</h2>
       <p>Av. Ciudad de Valparaíso 4236 Oficina 5, Cba, Argentina.</p>
       <p>+54 9 3512389500</p>
       <p>
