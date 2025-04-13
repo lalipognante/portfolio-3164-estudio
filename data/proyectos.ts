4eca0eca305c47912536befc59c81052ad3b8ea1
3164estudio <3164estudio@gmail.com>
2025-04-13 20:42:30 -0300

    Update proyectos.ts

--- data/proyectos.ts
+++ data/proyectos.ts
@@ -1,7 +1,25 @@
 export const proyectos = [
   {
+    titulo: "Proyecto 4",
+    descripcion: "Casa Cañuelas, Córdoba, 2020",
+    imagenes: [
+      "/proyectos/image1-project4.webp",
+      "/proyectos/image4-project4.webp",
+      "/proyectos/image5-project4.webp"
+    ]
+  },
+  {
+    titulo: "Proyecto Bustamante",
+    descripcion: "Edificio Bustamante, Córdoba, 2025",
+    imagenes: [
+      "/proyectos/Bustamante1.webp",
+      "/proyectos/Bustamante2.webp",
+      "/proyectos/Bustamante3.webp"
+    ]
+  },
+  {
     titulo: "Proyecto 1",
-    descripcion: "Refugio de Montaña, Potrero de Garay 2020",
+    descripcion: "Refugio de Montaña, Potrero de Garay, 2020",
     imagenes: [
       "/proyectos/image1-project1.webp",
       "/proyectos/image2-project1.webp",
@@ -10,6 +28,25 @@ export const proyectos = [
   },
   {
     titulo: "Proyecto 2",
+    descripcion: "Casa Hotel Estancia La Paz, 2020",
+    imagenes: [
+      "/proyectos/image1-project2.webp",
+      "/proyectos/image2-project2.webp"
+    ]
+  },
+  {
+    titulo: "Proyecto 5",
+    descripcion: "Casa Nueva Andalucia, 2024",
+    imagenes: [
+      "/proyectos/Andalucio1.webp",
+      "/proyectos/Andalucia2.webp",
+      "/proyectos/Andalucia3.webp",
+      "/proyectos/Andalucia4.webp",
+      "/proyectos/Andalucia5.webp",
+    ]
+  },
+  {
+    titulo: "Proyecto 6",
     descripcion: "Casa entre los árboles",
     imagenes: [
       "/proyectos/image1-project2.webp",
@@ -17,29 +54,22 @@ export const proyectos = [
     ]
   },
   {
-    titulo: "Proyecto 3",
-    descripcion: "Obra en las sierras",
+    titulo: "Proyecto 11",
+    descripcion: "Edificio Estrada, 2025",
+    imagenes: [
+      "/proyectos/estrada1.webp",
+      "/proyectos/estrada2.webp",
+      "/proyectos/estrada3.webp",
+      "/proyectos/estrada4.webp",
+      "/proyectos/estrada5.webp"
+    ]
+  },
+  {
+    titulo: "Proyecto 7",
+    descripcion: "Casa La Cumbrecita, 2023",
     imagenes: [
       "/proyectos/image1-project3.webp",
       "/proyectos/image2-project3.webp"
-    ]
-  },
-  {
-    titulo: "Proyecto 4",
-    descripcion: "Casa suburbana",
-    imagenes: [
-      "/proyectos/image1-project4.webp",
-      "/proyectos/image4-project4.webp",
-      "/proyectos/image5-project4.webp"
-    ]
-  },
-  {
-    titulo: "Proyecto 5",
-    descripcion: "Edificio Bustamante, 2025",
-    imagenes: [
-      "/proyectos/Bustamante1.webp",
-      "/proyectos/Bustamante2.webp",
-      "/proyectos/Bustamante3.webp"
     ]
   }
 ];
