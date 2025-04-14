af26fddc04ea7a99c3bac9d250af6d0233bc768e
3164estudio <3164estudio@gmail.com>
2025-04-14 16:08:19 -0300

    Update page.tsx

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,8 +1,7 @@
-
 export default function TeamPage() {
   return (
     <main className="max-w-2xl text-sm">
-      <ul className="list-disc list-inside">
+      <ul className="list-none">
         <li>De Biasi Ignacio</li>
         <li>Pognante Mariano</li>
         <li>Taravella Francisco</li>
