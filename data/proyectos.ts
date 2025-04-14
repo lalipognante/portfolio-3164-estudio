df73d46f3925384f15767e6831ba99227fd14c09
3164estudio <3164estudio@gmail.com>
2025-04-14 17:54:49 -0300

    Update proyectos.ts

--- data/proyectos.ts
+++ data/proyectos.ts
@@ -70,5 +70,29 @@ export const proyectos = [
   },
   {
     titulo: "Proyecto 13",
-    descripcion:
-
+    descripcion: "Mobiliario Rack TV, 2025",
+    imagenes: [
+      "/proyectos/Racktv01.webp"
+    ]
+  },
+  {
+    titulo: "Proyecto greenville",
+    descripcion: "Casa Greenville II, 2025",
+    imagenes: [
+      "/proyectos/Greenville1.webp",
+      "/proyectos/Greenville2.webp",
+      "/proyectos/Greenville3.webp",
+      "/proyectos/Greenville4.webp"
+    ]
+  },
+  {
+    titulo: "Proyecto molvento",
+    descripcion: "Casa Molvento, 2024",
+    imagenes: [
+      "/proyectos/Molvento1.webp",
+      "/proyectos/Molvento2.webp",
+      "/proyectos/Molvento3.webp",
+      "/proyectos/Molvento4.webp"
+    ]
+  }
+];
