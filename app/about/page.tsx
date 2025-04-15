ee7d6ce3a0123be0c3a09eacb580972f8bf645c0
Pognante Laura <lalipognante@gmail.com>
2025-04-14 23:39:33 -0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,28 @@
+"use client";
+
+import Image from "next/image";
 
 export default function AboutPage() {
   return (
-    <main className="max-w-2xl text-sm leading-relaxed">
+    <main className="max-w-2xl text-sm leading-relaxed space-y-6">
       <p>
         31 64 estudio es oficina de arquitectura fundada en 2024 en Córdoba Capital, Argentina,
         comprometida con el diseño de espacios habitables y su relación con el entorno. Su nombre responde a las
         coordenadas geográficas 31° sur y 64° oeste, haciendo del contexto una reflexión primordial e ineludible.
       </p>
-      <p className="mt-4">
+      <p>
         Abordamos el proceso arquitectónico en todas sus etapas con una búsqueda del manejo de la técnica y conscientes del paso del tiempo en el objeto construido. Creemos en la necesaria integridad entre programa, forma y contexto. La sensibilidad hacia el ser humano, pero también en los mecanismos de construcción con el anhelo de dominar el arte de construir.
       </p>
+
+      <div className="flex justify-center">
+        <Image
+          src="/oficina.webp"
+          alt="Imagen de la oficina"
+          width={800}
+          height={500}
+          className="rounded-xl shadow-md w-full h-auto"
+        />
+      </div>
     </main>
   );
 }
