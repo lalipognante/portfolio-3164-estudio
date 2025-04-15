c2e14ef46a1c596fac9d22e0b74d8566744cfd0c
Pognante Laura <lalipognante@gmail.com>
2025-04-14 23:37:49 -0300

    Update page.tsx

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,11 +1,25 @@
+"use client";
+
+import Image from "next/image";
+
 export default function TeamPage() {
   return (
-    <main className="max-w-2xl text-sm">
+    <main className="max-w-2xl text-sm space-y-4">
       <ul className="list-none">
         <li>De Biasi Ignacio</li>
         <li>Pognante Mariano</li>
         <li>Taravella Francisco</li>
       </ul>
+
+      <div className="flex justify-center">
+        <Image
+          src="/team.webp"
+          alt="Foto del equipo"
+          width={600}
+          height={400}
+          className="rounded-xl shadow-md"
+        />
+      </div>
     </main>
   );
 }
